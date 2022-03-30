42d87ede3ea1a51dfb04af21c4afd75eb16e26a9
Eric McGary <eric@rhove.com>
2022-03-29 23:17:35 -0400

    remove unused code

--- Sources/CustomAuthSolanaAdapter/CustomAuthSolanaAdapter.swift
+++ Sources/CustomAuthSolanaAdapter/CustomAuthSolanaAdapter.swift
@@ -7,41 +7,7 @@ import FetchNodeDetails
 import PromiseKit
 
 public extension CustomAuth {
-    
-//    /// Initiate an CustomAuth instance.
-//    /// - Parameters:
-//    ///   - aggregateVerifierType: Type of the verifier. Use `singleLogin` for single providers. Only `singleLogin` and `singleIdVerifier` is supported currently.
-//    ///   - aggregateVerifierName: Name of the verifier to be used..
-//    ///   - subVerifierDetails: Details of each subverifiers to be used.
-//    ///   - factory: Providng mocking by implementing TDSDKFactoryProtocol.
-//    ///   - ethereumNetwork: Etherum network to be used.
-//    ///   - solanaNetwork: Solana network to be used.
-//    ///   - loglevel: Indicates the log level of this instance. All logs lower than this level will be ignored.
-//    convenience init(
-//        aggregateVerifierType: verifierTypes,
-//        aggregateVerifierName: String,
-//        subVerifierDetails: [SubVerifierDetails],
-//        factory: CASDKFactoryProtocol = CASDKFactory(),
-//        ethereumNetwork: EthereumNetwork = .MAINNET,
-//        solanaNetwork: Network = .mainnetBeta,
-//        loglevel: OSLogType = .debug,
-//        urlSession: URLSession = URLSession.shared
-//    ) {
-//
-//        network = solanaNetwork
-//
-//        self.init(
-//            aggregateVerifierType: aggregateVerifierType,
-//            aggregateVerifierName: aggregateVerifierName,
-//            subVerifierDetails: subVerifierDetails,
-//            factory: factory,
-//            network: ethereumNetwork,
-//            loglevel: loglevel,
-//            urlSession: urlSession
-//        )
-//
-//    }
-//
+ 
     /// Retrieve the Torus key from the nodes given an already known token. Useful if a custom login flow is required.
     /// - Parameters:
     ///   - verifier: A verifier is a unique identifier for your OAuth registration on the torus network. The public/private keys generated for a user are scoped to a verifier.
